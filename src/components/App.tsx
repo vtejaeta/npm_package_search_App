9c962e9494cc7150900659c666bde9c61c585851
vtejaeta <vtejaeta@gmail.com>
2021-01-23 10:23:58 +0530

    Wiring up of React

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,16 @@
-const App = () => {
-    return(
-        <h1>Helloo</h1>
-    )
+import { Provider } from 'react-redux'
+import { store } from '../redux'
+import RepositoriesList from './RepositoriesList'
 
+const App = () => {
+  return (
+      <Provider store={store}>
+          <div>
+            <h1>Search for Package</h1>
+            <RepositoriesList/>
+          </div>
+      </Provider>
+  )
 }
 
 export default App
