d00995653a900a0087aab8861a7f82da505367e8
vtejaeta <vtejaeta@gmail.com>
2021-01-23 10:10:12 +0530

    Setup of Action creators

--- src/redux/action-creators/index.ts
+++ src/redux/action-creators/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {Dispatch} from 'redux'
+import { Dispatch } from 'redux'
 import { ActionType } from '../action-types'
 import { Action } from '../actions'
 
@@ -19,7 +19,7 @@ export const searchRepositories = (searchTerm: string) => {
         }
       )
 
-      const names = data.objects.map((result) => {
+      const names = data.objects.map((result: any) => {
         return result.package.name
       })
 
