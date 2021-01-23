c16cd55df4a0f1c251d0eb2345434f1fd6899ad9
vtejaeta <vtejaeta@gmail.com>
2021-01-23 11:30:27 +0530

    hook to access diff action creators

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,8 +1,20 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { actionCreators } from '../redux'
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('')
+  const dispatch = useDispatch()
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    dispatch(actionCreators.searchRepositories(term))
+  }
+
   return (
     <div>
-      <form>
-        <input />
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
     </div>
---
+++ src/hooks/useActions.ts
@@ -0,0 +1,8 @@
+import { useDispatch } from 'react-redux'
+import { actionCreators } from '../redux'
+import { bindActionCreators } from 'redux'
+
+export const useActions = () => {
+  const dispatch = useDispatch()
+  return bindActionCreators(actionCreators, dispatch)
+}
